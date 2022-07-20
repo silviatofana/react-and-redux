fffeb65ef452263b68b80b7dd8ebed675b7c0cf8
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-20 09:36:46 +0300

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Rockets from './components/rockets/Rockets';
+import Missions from './components/missions/Missions';
+import Dragons from './components/dragons/Dragons';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
