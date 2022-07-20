8eb7480876bd1b5d2d62384bd8d182f7c0ada065
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-20 09:35:14 +0300

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable quotes */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "./Redux/ConfigureStore";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import store from './redux/configureStore';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <App />
       </Router>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
 reportWebVitals();
