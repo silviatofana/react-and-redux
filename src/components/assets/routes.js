c0fb74b681acf75cf16d129fbcb38647c34f0561
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-20 13:20:05 +0300

    linters

--- src/components/assets/routes.js
+++ src/components/assets/routes.js
@@ -1,9 +1,11 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
-import Missions from "../components/Missions/Missions";
-import Profile from "../components/Profile/profile";
-import Rockets from "../components/Rockets/Rockets";
+// import Missions from "../components/Missions/Missions";
+// import Profile from "../components/Profile/profile";
+// import Rockets from "../components/Rockets/Rockets";
+
+import Missions from "../Missions/Missions";
 
 const routes = [
   {
