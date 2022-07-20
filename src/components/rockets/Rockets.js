ce3da555d38a133fcb9002a79af030244487daaa
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-20 11:50:22 +0300

    routes.js/assets

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {getRocketsFromApi } from './redux/rockets/rockets';
+import { getRocketsFromAPI } from './redux/rockets/rockets';
 
 const Rockets = () => {
   const dispatch = useDispatch();
