b89d6f2b1d3f7a9d2b864959eb0c35450fd73592
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-20 09:41:32 +0300

    update configureStore.js

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,15 +1,17 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable quotes */
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import RocketsReducer from "./rockets";
+import rocketsReducer from './rockets/rockets';
+import dragonReducer from './dragons/dragons';
+import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
-  rockets: RocketsReducer.reducer,
+  missions: missionsReducer,
+  dragons: dragonReducer,
+  rockets: rocketsReducer,
 });
+
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
